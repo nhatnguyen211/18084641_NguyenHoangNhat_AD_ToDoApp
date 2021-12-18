553788f7b75b1394418ae92022eb7d54cafc836f
nhatnguyen211 <80726326+nhatnguyen211@users.noreply.github.com>
2021-12-19 00:23:06 +0700

    update

--- app/src/main/java/com/example/nguyenhoangnhat_todoapp/FirebaseUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nguyenhoangnhat_todoapp
-
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-
-object FirebaseUtils {
-    val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
-}
--- app/src/main/java/com/example/nguyenhoangnhat_todoapp/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.nguyenhoangnhat_todoapp
-
-data class User(var work:String?=null, var status:String?=null)
